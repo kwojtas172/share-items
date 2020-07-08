6a144765d634776eaf6d8d46235c6de96fab0b11
kwojtas172 <k.wojtas172@gmail.com>
2020-07-08 20:41:25 +0200

    created HomeAboutUs

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import decoration from "../images/Decoration.svg";
 
 export default class HomeAboutUs extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <section className="home-about-us" id="about-us">
+                <div className="home-about-us__text">
+                    <h3>O nas</h3>
+                    <img src={decoration} alt="decoration-line" />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ante metus dictum at tempor commodo ullamcorper a. Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Dui accumsan sit amet nulla facilisi morbi. </p>
+                </div>
+                <div className="home-about-us__img"></div>
+            </section>
         )
     }
 }
