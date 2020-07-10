65b025957062b082607e629a910523805fac5692
kwojtas172 <k.wojtas172@gmail.com>
2020-07-10 15:31:10 +0200

    Updates

---
+++ precache-manifest.e9ab555c5e2533a16fbfce92593c90ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5cfd6f2f6c997985d772d083298732",
+    "url": "/share-items/index.html"
+  },
+  {
+    "revision": "88bf0761c6ab0ffbede0",
+    "url": "/share-items/static/css/main.72eda679.chunk.css"
+  },
+  {
+    "revision": "334a0b8cab6a0f979c35",
+    "url": "/share-items/static/js/2.268fa7f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/share-items/static/js/2.268fa7f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88bf0761c6ab0ffbede0",
+    "url": "/share-items/static/js/main.7fdd0650.chunk.js"
+  },
+  {
+    "revision": "7435408c979ec04f561b",
+    "url": "/share-items/static/js/runtime-main.0ad33284.js"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/share-items/static/media/3 Columns Background.99f2e620.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/share-items/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/share-items/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/share-items/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/share-items/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/share-items/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/share-items/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/share-items/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/share-items/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/share-items/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/share-items/static/media/People.6fa36651.jpg"
+  }
+]);
